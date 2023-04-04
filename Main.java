989fb1c145d2dbb7a773e223651fb894b35c919a
Brian Schultz <68474136+BrianSchultz1@users.noreply.github.com>
2023-04-03 23:24:02 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite seu nome: ");
+        System.out.println("Digite seu nome: "); 
         String nome = scan.nextLine();
 
 
